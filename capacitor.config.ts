9328f5178670396e34d799dceb5fe9869085004c
shridhiaggarwal <shridhi.aggarwal@gmail.com>
2025-05-05 22:07:37 +0530

    project setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.learning.tinderswipeapp',
+  appName: 'tinder-swipe-app',
+  webDir: 'build'
+};
+
+export default config;
